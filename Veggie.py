b2584c6d29c150a478fa1309b6869dd5f4c2eda9
noob-bee <viksbsingh@gmail.com>
2023-12-07 10:35:11 -0500

    Access modifiers of Veggie

--- Veggie.py
+++ Veggie.py
@@ -4,20 +4,20 @@ class Veggie(FieldInhabitant):
 
     def __init__(self, veg_name, veg_symbol, veg_points):
         super().__init__(veg_symbol)
-        self.__veg_name = veg_name
-        self.__veg_points = veg_points
+        self.veg_name = veg_name
+        self.veg_points = veg_points
 
     def setVegName(self, veg_name):
-        self.__veg_name = veg_name
+        self.veg_name = veg_name
 
     def setVegPoints(self, veg_points):
-        self.__veg_points = veg_points
+        self.veg_points = veg_points
 
     def getVegName(self):
-        return self.__veg_name
+        return self.veg_name
 
     def getVegPoints(self):
-        return self.__veg_points
+        return self.veg_points
 
     def __str__(self):
         print(f"Vegetable Symbol: {self.getSymbol()}, Vegetable Points: {self.getVegPoints()}, Vegetable Name: {self.getVegName()}")
